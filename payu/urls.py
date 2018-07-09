c5eba47e0590d933b8a83981629cf4ba4a987f42
piotrkurnik <piotr.kurnik@10clouds.com>
2018-07-09 16:33:54 +0200

    Updates for Django 2.0

--- payu/urls.py
+++ payu/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 
-
+app_name = 'payu'
 urlpatterns = [
     url(r'^api/', include('payu.urls_api', namespace='api'))
 ]
--- payu/urls_api.py
+++ payu/urls_api.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 # callable views
 from .api import notify
 
-
+app_name = 'api'
 urlpatterns = [
     url(r'^notify/$', notify, name='notify')
 ]
